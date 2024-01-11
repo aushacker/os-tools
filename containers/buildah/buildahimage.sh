43ba25697843529e2962b187300d8e7729bf92e7
aushacker <25114336+aushacker@users.noreply.github.com>
2024-01-11 13:21:00 +1100

    improved ubi-micro build

--- containers/buildah/buildahimage.sh
+++ containers/buildah/buildahimage.sh
@@ -2,7 +2,7 @@
 ctr=$(buildah from registry.access.redhat.com/ubi8/ubi-micro)
 mnt=$(buildah mount $ctr)
 dnf upgrade -y --installroot $mnt --disableplugin=subscription-manager
-dnf install -y --installroot $mnt --disableplugin=subscription-manager --setopt=tsflags=nodocs bind-utils
+dnf install -y --installroot $mnt --disableplugin=subscription-manager --setopt=tsflags=nodocs ${PACKAGES}
 dnf clean all --installroot $mnt --disableplugin=subscription-manager
 #buildah config --entrypoint /bin/bash $ctr
 buildah commit $ctr localhost/myhttp
